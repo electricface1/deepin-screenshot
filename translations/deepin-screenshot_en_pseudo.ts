e36199b4ef9f8ff41cee588edf6dd123c8ff6a7c
electricface1 <weima93@outlook.com>
2019-03-15 16:17:33 +0800

    chore: auto pull translation files from transifex

---
+++ translations/deepin-screenshot_en_pseudo.ts
@@ -0,0 +1,289 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="641"/>
+        <source>select-area</source>
+        <translation>[şḗŀḗƈŧ-ȧřḗȧ ϱϱϱ衋ϰẛ靐ΐϱΰΰ]</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="986"/>
+        <location filename="../mainwindow.cpp" line="988"/>
+        <location filename="../mainwindow.cpp" line="1246"/>
+        <location filename="../mainwindow.cpp" line="1249"/>
+        <location filename="../mainwindow.cpp" line="1262"/>
+        <location filename="../mainwindow.cpp" line="1265"/>
+        <location filename="../mainwindow.cpp" line="1302"/>
+        <location filename="../mainwindow.cpp" line="1305"/>
+        <location filename="../mainwindow.cpp" line="1337"/>
+        <location filename="../mainwindow.cpp" line="1340"/>
+        <source>DeepinScreenshot</source>
+        <translation>[ḒḗḗƥīƞŞƈřḗḗƞşħǿŧ ϵϰϕϐǋϑ靐鶱ſϐϵ]</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1004"/>
+        <location filename="../mainwindow.cpp" line="1366"/>
+        <source>View</source>
+        <translation>[Ṽīḗẇ ΰςΐϕ]</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1018"/>
+        <location filename="../mainwindow.cpp" line="1386"/>
+        <source>Picture has been saved to %1</source>
+        <translation>[Ƥīƈŧŭřḗ ħȧş ƀḗḗƞ şȧṽḗḓ ŧǿ %1 ǅıϑΐſϐϕ 鶱ϱϖ]</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1270"/>
+        <source>PNG (*.png);;JPEG (*.jpg *.jpeg);; BMP (*.bmp);; PGM (*.pgm);;XBM (*.xbm);;XPM(*.xpm)</source>
+        <translation>[ƤȠƓ (*.ƥƞɠ);;ĴƤḖƓ (*.ĵƥɠ *.ĵƥḗɠ);; ƁḾƤ (*.ƀḿƥ);; ƤƓḾ (*.ƥɠḿ);;ẊƁḾ (*.ẋƀḿ);;ẊƤḾ(*.ẋƥḿ) ϐ靐ẛ ςΰẛϐϵϑ衋鶱ΰΐ]</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1360"/>
+        <source>Save failed. Please save it in your home directory.</source>
+        <translation>[Şȧṽḗ ƒȧīŀḗḓ. Ƥŀḗȧşḗ şȧṽḗ īŧ īƞ ẏǿŭř ħǿḿḗ ḓīřḗƈŧǿřẏ. ǲςϐ衋 ϖϐ]</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="1384"/>
+        <source>Picture has been saved to clipboard</source>
+        <translation>[Ƥīƈŧŭřḗ ħȧş ƀḗḗƞ şȧṽḗḓ ŧǿ ƈŀīƥƀǿȧřḓ ſϵ衋ϕϱϕϱ 衋ϵı]</translation>
+    </message>
+</context>
+<context>
+    <name>MenuController</name>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="40"/>
+        <source>Rectangle</source>
+        <translation>[Řḗƈŧȧƞɠŀḗ ϑΰϱςϰǅΰϕϰ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="48"/>
+        <source>Ellipse</source>
+        <translation>[Ḗŀŀīƥşḗ ǲſϖΰΰẛϐ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="56"/>
+        <source>Arrow</source>
+        <translation>[Ȧřřǿẇ ϐϰϕſǲ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="64"/>
+        <source>Pencil</source>
+        <translation>[Ƥḗƞƈīŀ ϖẛẛϑϱǅ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="72"/>
+        <source>Text</source>
+        <translation>[Ŧḗẋŧ ǋϑϖǋ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="80"/>
+        <source>Undo</source>
+        <translation>[Ŭƞḓǿ ǋǋſ衋]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="96"/>
+        <source>Save</source>
+        <translation>[Şȧṽḗ ϕϑΰϑ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="99"/>
+        <source>Save to desktop</source>
+        <translation>[Şȧṽḗ ŧǿ ḓḗşķŧǿƥ ſϖıϰ 衋ϰ ϵϱ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="100"/>
+        <source>Autosave</source>
+        <translation>[Ȧŭŧǿşȧṽḗ ıϰϕẛǈẛſϐ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="101"/>
+        <source>Save to specified folder</source>
+        <translation>[Şȧṽḗ ŧǿ şƥḗƈīƒīḗḓ ƒǿŀḓḗř ıǈſς ǋ衋 ΰ靐]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="102"/>
+        <source>Copy to clipboard</source>
+        <translation>[Ƈǿƥẏ ŧǿ ƈŀīƥƀǿȧřḓ 靐ϰǲΰ ϵǈ 鶱ϐϱϵ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="103"/>
+        <source>Autosave and copy to clipboard</source>
+        <translation>[Ȧŭŧǿşȧṽḗ ȧƞḓ ƈǿƥẏ ŧǿ ƈŀīƥƀǿȧřḓ ı鶱ϕıΐı鶱ΐ ǈ]</translation>
+    </message>
+    <message>
+        <location filename="../controller/menucontroller.cpp" line="124"/>
+        <source>Exit</source>
+        <translation>[Ḗẋīŧ ǅǋǈϰ]</translation>
+    </message>
+</context>
+<context>
+    <name>Screenshot</name>
+    <message>
+        <location filename="../screenshot.cpp" line="78"/>
+        <source>Deepin Screenshot will start after %1 seconds</source>
+        <translation>[Ḓḗḗƥīƞ Şƈřḗḗƞşħǿŧ ẇīŀŀ şŧȧřŧ ȧƒŧḗř %1 şḗƈǿƞḓş ς鶱ϵΐıǋ 鶱ΐǈ]</translation>
+    </message>
+</context>
+<context>
+    <name>Shortcut</name>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="29"/>
+        <source>Start/Screenshot</source>
+        <translation>[Şŧȧřŧ/Şƈřḗḗƞşħǿŧ ϵǈϱΰǲϖϖı鶱ϕϑ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="30"/>
+        <location filename="../utils/shortcut.cpp" line="41"/>
+        <source>Save</source>
+        <translation>[Şȧṽḗ ςϵϕς]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="31"/>
+        <source>Painting</source>
+        <translation>[Ƥȧīƞŧīƞɠ ΐϑϕϑſϐϵ衋]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="32"/>
+        <source>Size Adjustment</source>
+        <translation>[Şīzḗ Ȧḓĵŭşŧḿḗƞŧ 衋ΰǅϐ ϕǲſ衋ϖ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="35"/>
+        <source>Quick start</source>
+        <translation>[Ɋŭīƈķ şŧȧřŧ 衋ϰẛϰ鶱 ςϕ衋ΐ靐]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="36"/>
+        <source>Full screenshot</source>
+        <translation>[Ƒŭŀŀ şƈřḗḗƞşħǿŧ ϖıϖſ ſϱǲ衋ϖ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="37"/>
+        <source>Delay screenshot</source>
+        <translation>[Ḓḗŀȧẏ şƈřḗḗƞşħǿŧ 衋ϕϵſǅ ǲϕ靐鶱ϱ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="38"/>
+        <source>Undo</source>
+        <translation>[Ŭƞḓǿ ǲıϖϱ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="39"/>
+        <source>Exit</source>
+        <translation>[Ḗẋīŧ 靐ſϑ衋]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="42"/>
+        <source>Copy to clipboard</source>
+        <translation>[Ƈǿƥẏ ŧǿ ƈŀīƥƀǿȧřḓ 衋ϵΰϵ ẛſ ϐϰ鶱ϰ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="45"/>
+        <source>Rectangle</source>
+        <translation>[Řḗƈŧȧƞɠŀḗ 靐ϵϵẛϐıϐıſ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="46"/>
+        <source>Ellipse</source>
+        <translation>[Ḗŀŀīƥşḗ ǲ衋鶱ẛ靐衋ϕ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="47"/>
+        <source>Arrow</source>
+        <translation>[Ȧřřǿẇ ǅϖΐẛϐ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="48"/>
+        <source>Pencil</source>
+        <translation>[Ƥḗƞƈīŀ ϑǅǅϐϕϖ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="49"/>
+        <source>Text</source>
+        <translation>[Ŧḗẋŧ ẛϱ靐靐]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="50"/>
+        <source>Color</source>
+        <translation>[Ƈǿŀǿř 衋ǲ靐ΐϑ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="53"/>
+        <source>Increase height up</source>
+        <translation>[Īƞƈřḗȧşḗ ħḗīɠħŧ ŭƥ ſǅ鶱ıϖςςΐ ΐ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="54"/>
+        <source>Increase height down</source>
+        <translation>[Īƞƈřḗȧşḗ ħḗīɠħŧ ḓǿẇƞ ϵϰΐǲϕϕΐϖ 衋ΐΰ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="55"/>
+        <source>Increase width left</source>
+        <translation>[Īƞƈřḗȧşḗ ẇīḓŧħ ŀḗƒŧ ı鶱ΰϐǲϱǲ衋 ẛϕ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="56"/>
+        <source>Increase width right</source>
+        <translation>[Īƞƈřḗȧşḗ ẇīḓŧħ řīɠħŧ 衋ςϑıẛ靐ſ鶱 ϱ鶱ϑ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="57"/>
+        <source>Decrease height up</source>
+        <translation>[Ḓḗƈřḗȧşḗ ħḗīɠħŧ ŭƥ ſǅϑϰϱϑϑ靐 ϱ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="58"/>
+        <source>Decrease height down</source>
+        <translation>[Ḓḗƈřḗȧşḗ ħḗīɠħŧ ḓǿẇƞ ſϱϖıſςıϕ 衋ſǲ]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="59"/>
+        <source>Decrease width left</source>
+        <translation>[Ḓḗƈřḗȧşḗ ẇīḓŧħ ŀḗƒŧ ϵΰϖǅǅϱΰǈ ǲ衋]</translation>
+    </message>
+    <message>
+        <location filename="../utils/shortcut.cpp" line="60"/>
+        <source>Decrease width right</source>
+        <translation>[Ḓḗƈřḗȧşḗ ẇīḓŧħ řīɠħŧ ϵϐςǲǲϱſϱ ΐǈϕ]</translation>
+    </message>
+</context>
+<context>
+    <name>SubToolBar</name>
+    <message>
+        <location filename="../widgets/subtoolbar.cpp" line="397"/>
+        <source>Save to desktop</source>
+        <translation>[Şȧṽḗ ŧǿ ḓḗşķŧǿƥ ıǈϕǲ ǋϐ ϐǲ]</translation>
+    </message>
+    <message>
+        <location filename="../widgets/subtoolbar.cpp" line="403"/>
+        <source>Autosave</source>
+        <translation>[Ȧŭŧǿşȧṽḗ ϵıϖ衋ǅϰſǈ]</translation>
+    </message>
+    <message>
+        <location filename="../widgets/subtoolbar.cpp" line="409"/>
+        <source>Save to specified folder</source>
+        <translation>[Şȧṽḗ ŧǿ şƥḗƈīƒīḗḓ ƒǿŀḓḗř ǈǈϰϱ 靐ſ ϑϖ]</translation>
+    </message>
+    <message>
+        <location filename="../widgets/subtoolbar.cpp" line="415"/>
+        <source>Copy to clipboard</source>
+        <translation>[Ƈǿƥẏ ŧǿ ƈŀīƥƀǿȧřḓ ϑϖ衋鶱 ϱς 衋ϖΐǋ]</translation>
+    </message>
+    <message>
+        <location filename="../widgets/subtoolbar.cpp" line="420"/>
+        <source>Autosave and copy to clipboard</source>
+        <translation>[Ȧŭŧǿşȧṽḗ ȧƞḓ ƈǿƥẏ ŧǿ ƈŀīƥƀǿȧřḓ ςǅǈǋΐΰǋı ſ]</translation>
+    </message>
+    <message>
+        <location filename="../widgets/subtoolbar.cpp" line="425"/>
+        <source>Low</source>
+        <translation>[Ŀǿẇ ſẛǅ]</translation>
+    </message>
+    <message>
+        <location filename="../widgets/subtoolbar.cpp" line="439"/>
+        <source>High</source>
+        <translation>[Ħīɠħ ǋϐẛΐ]</translation>
+    </message>
+</context>
+</TS>
